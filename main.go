50bc78ac56562e1e304010294c6cbb1caeaae431
Nick Klauer <nklauer@zendesk.com>
2020-01-29 16:22:52 -0600

    fix: stupid parallelism

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/google/go-github/v29/github"
 	cli "github.com/urfave/cli/v2"
@@ -93,27 +94,33 @@ func runApp(c *cli.Context) error {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
+	var wg sync.WaitGroup
 	for _, team := range settings.TeamPermissions {
-		t, _, err := client.Teams.GetTeamBySlug(ctx, settings.Organization, team.Team)
-		if err != nil {
-			return fmt.Errorf("Unable to get Team from organization: %w", err)
-		}
-		fmt.Printf("Team: %v ID: %v\n", team.Team, *t.ID)
-		team.ID = *t.ID
-
+		wg.Add(1)
+		go getTeamSlug(&wg, client, ctx, settings, team)
 	}
+	wg.Wait()
 
 	for _, repo := range settings.Repositories {
 		if len(settings.TeamPermissions) > 0 {
 			for _, perm := range settings.TeamPermissions {
 				fmt.Printf("Repo: %v, Permission: %+v\n", repo, perm)
-				if err = addPermissions(client, ctx, repo, settings.Organization, *perm); err != nil {
-					return err
-				}
-
+				go addPermissions(client, ctx, repo, settings.Organization, *perm)
 			}
 		}
 	}
 
 	return nil
 }
+
+func getTeamSlug(wg *sync.WaitGroup, client *github.Client, ctx context.Context, settings *PermissionsSettings, team *Permissions) error {
+	defer wg.Done()
+	t, _, err := client.Teams.GetTeamBySlug(ctx, settings.Organization, team.Team)
+	if err != nil {
+		return fmt.Errorf("Unable to get Team from organization: %w", err)
+	}
+	fmt.Printf("Team: %v ID: %v\n", team.Team, *t.ID)
+	team.ID = *t.ID
+	return nil
+}
+
