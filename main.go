7cb46b36237de57a3b48e6a6e9d98562fe84c7a6
Nick Klauer <nklauer@zendesk.com>
2020-02-23 15:34:09 -0600

    chore: rearrange code

--- main.go
+++ main.go
@@ -67,19 +67,6 @@ func main() {
 
 }
 
-func addPermissions(client *github.Client, ctx context.Context, repo string, organization string, perm Permissions) error {
-	resp, err := client.Teams.AddTeamRepoBySlug(ctx, organization, perm.Team, organization, repo, &github.TeamAddTeamRepoOptions{Permission: string(perm.Level)})
-	if err != nil {
-		fmt.Printf("error adding %v as collaborator to %v: %v\n", perm.Team, repo, resp.Status)
-		resp, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return fmt.Errorf("Unable to read response body: %w", err)
-		}
-		fmt.Println(string(resp))
-	}
-	return nil
-}
-
 func runApp(c *cli.Context) error {
 	file, err := ioutil.ReadFile(repositoriesYAMLConfig)
 	if err != nil {
@@ -124,63 +111,6 @@ func runApp(c *cli.Context) error {
 				fmt.Println(err)
 			}
 		}
-	}
-	return nil
-}
-
-func getTeamSlug(client *github.Client, ctx context.Context, settings *PermissionsSettings, team *Permissions) error {
-	t, _, err := client.Teams.GetTeamBySlug(ctx, settings.Organization, team.Team)
-	if err != nil {
-		return fmt.Errorf("Unable to get Team from organization: %w", err)
-	}
-	fmt.Printf("Team: %v ID: %v\n", team.Team, *t.ID)
-	team.ID = *t.ID
-	return nil
-}
-
-func GetRepository(client *githubv4.Client, ctx context.Context, name, owner string) (*githubv4.ID, error) {
-	var query struct {
-		Repository struct {
-			ID                 githubv4.ID
-			HasWikiEnabled     githubv4.Boolean
-			HasIssuesEnabled   githubv4.Boolean
-			HasProjectsEnabled githubv4.Boolean
-		} `graphql:"repository(owner: $owner, name: $name)"`
-	}
-	err := client.Query(ctx, &query, map[string]interface{}{
-		"owner": githubv4.String(owner),
-		"name":  githubv4.String(name),
-	})
-	if err != nil {
-		fmt.Printf("error retrieving %s/%s: %v\n", owner, name, err)
-		return nil, err
-	}
-	fmt.Printf("repository %s/%s: wiki:%v,issues:%v,projects:%v\n", owner, name, query.Repository.HasWikiEnabled, query.Repository.HasIssuesEnabled, query.Repository.HasProjectsEnabled)
-	return &query.Repository.ID, nil
-}
-
-func SetRepository(client *githubv4.Client, ctx context.Context, id githubv4.ID, wiki, issues, project bool) error {
-	var mutation struct {
-		UpdateRepository struct {
-			ClientMutationID githubv4.String
-			Repository struct {
-				ID githubv4.ID
-				HasWikiEnabled githubv4.Boolean
-				HasIssuesEnabled githubv4.Boolean
-				HasProjectsEnabled githubv4.Boolean
-			}
-		} `graphql:"updateRepository(input: $input)"`
-	}
-	input := githubv4.UpdateRepositoryInput{
-		RepositoryID:       id,
-		HasWikiEnabled:     githubv4.NewBoolean(githubv4.Boolean(wiki)),
-		HasIssuesEnabled:   githubv4.NewBoolean(githubv4.Boolean(issues)),
-		HasProjectsEnabled: githubv4.NewBoolean(githubv4.Boolean(project)),
-	}
-	err := client.Mutate(ctx, &mutation, input, nil)
-	if err != nil {
-		fmt.Printf("error updating repository ID %v: %v", id, err)
-		return err
 	}
 	return nil
 }
