eea92e4dd4cac5009ca11c59ec77bbe0cd2bd845
Nick Klauer <nklauer@zendesk.com>
2020-06-26 22:17:27 -0500

    ✨ pull request protections
    disallow rebase, allow squash commits, etc.

--- github_v3.go
+++ github_v3.go
@@ -14,17 +14,22 @@ import (
 )
 
 type GitHubClient struct {
-	Teams   TeamsService
-	Graph   GraphQLClient
-	V3      *github.Client
-	V4      *githubv4.Client
-	Context context.Context
+	Teams        TeamsService
+	Repositories RepositoriesService
+	Graph        GraphQLClient
+	V3           *github.Client
+	V4           *githubv4.Client
+	Context      context.Context
 }
 
 // TeamsService is a wrapepr interface for the GitHub V3 API to support mocking and testing.
 type TeamsService interface {
 	GetTeamBySlug(ctx context.Context, org, slug string) (*github.Team, *github.Response, error)
 	AddTeamRepoBySlug(ctx context.Context, org, slug, owner, repo string, opts *github.TeamAddTeamRepoOptions) (*github.Response, error)
+}
+
+type RepositoriesService interface {
+	Edit(ctx context.Context, org, repo string, repository *github.Repository) (*github.Repository, *github.Response, error)
 }
 
 func NewGitHubClient(ctx context.Context, staticToken string) *GitHubClient {
@@ -36,11 +41,12 @@ func NewGitHubClient(ctx context.Context, staticToken string) *GitHubClient {
 	clientV4 := githubv4.NewClient(tc)
 
 	return &GitHubClient{
-		Teams:   client.Teams,
-		V3:      client,
-		V4:      clientV4,
-		Graph:   clientV4,
-		Context: ctx,
+		Teams:        client.Teams,
+		Repositories: client.Repositories,
+		V3:           client,
+		V4:           clientV4,
+		Graph:        clientV4,
+		Context:      ctx,
 	}
 }
 
@@ -59,14 +65,14 @@ func (c *GitHubClient) AddPermissions(organization, repo string, perm Permission
 			Str("repo", repo).
 			Str("response-status", resp.Status).
 			Msg("error adding team as collaborator to repo")
-		resp, err := ioutil.ReadAll(resp.Body)
+		respBody, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Err(err).
 				Msg("unable to read response body")
 			return fmt.Errorf("unable to read response body: %w", err)
 		}
 		log.Debug().
-			Str("response-body", string(resp))
+			Str("response-body", string(respBody))
 	}
 	log.Info().Fields(map[string]interface{}{
 		"code": resp.StatusCode,
@@ -82,5 +88,30 @@ func (c *GitHubClient) SetTeamSlug(settings *PermissionsSettings, team *Permissi
 	}
 	fmt.Printf("Team: %v ID: %v\n", team.Team, *t.ID)
 	team.ID = *t.ID
+	return nil
+}
+
+func (c *GitHubClient) UpdateRepositorySettings(org, repo string, perms *BranchPermissions) error {
+	_, resp, err := c.Repositories.Edit(c.Context, org, repo, &github.Repository{
+		AllowMergeCommit: github.Bool(perms.AllowMergeCommit),
+		AllowRebaseMerge: github.Bool(perms.AllowRebaseMerge),
+		AllowSquashMerge: github.Bool(perms.AllowMergeCommit),
+	})
+	if err != nil {
+		log.Err(err).
+			Str("org", org).Str("repo", repo).Str("repsonse-status", resp.Status).Msg("Error updating repository settings")
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Err(err).
+				Msg("unable to read response body")
+			return fmt.Errorf("unable to read response body: %w", err)
+		}
+		log.Debug().Str("response-body", string(respBody))
+	}
+
+	log.Info().Fields(map[string]interface{}{
+		"code": resp.StatusCode,
+	})
+
 	return nil
 }
