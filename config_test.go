7b98e317d972b5abfb1bc37a9dc1cfce3aa68ffe
Nick Klauer <nklauer@zendesk.com>
2020-06-29 14:49:40 -0500

    ✅ add more test stubs

--- config_test.go
+++ config_test.go
@@ -21,3 +21,21 @@ func TestMapPermissions(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateBranchMergeStrategies(t *testing.T) {
+	type args struct {
+		settings *PermissionsSettings
+		client   *GitHubClient
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			UpdateBranchMergeStrategies(tt.args.settings, tt.args.client)
+		})
+	}
+}
