5eb02f527bb2ba627dc561a96804f4817eb3da40
Nick Klauer <nklauer@zendesk.com>
2020-07-05 20:14:55 -0500

    🎨 move mocking helpers to their own file
    separate out the reusable bits for mocking

---
+++ mocking_test.go
@@ -0,0 +1,71 @@
+package ownershit
+
+import (
+	"context"
+	"flag"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-github/v32/github"
+	"github.com/klauern/ownershit/mocks"
+	"github.com/rs/zerolog"
+)
+
+func stringPtr(s string) *string {
+	return &s
+}
+
+func boolPtr(b bool) *bool {
+	return &b
+}
+
+func defaultGitHubClient() *GitHubClient {
+	return NewGitHubClient(context.TODO(), "")
+}
+
+var defaultGoodResponse = &github.Response{
+	Response: &http.Response{
+		StatusCode: 200,
+	},
+}
+
+var (
+	githubv3 = flag.Bool("githubv3", false, "run GitHub V3 Integration Tests")
+)
+
+func TestMain(m *testing.M) {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	flag.Parse()
+	result := m.Run()
+	os.Exit(result)
+}
+
+type testMocks struct {
+	ctrl      *gomock.Controller
+	client    *GitHubClient
+	teamMock  *mocks.MockTeamsService
+	repoMock  *mocks.MockRepositoriesService
+	graphMock *mocks.MockGraphQLClient
+}
+
+func setupMocks(t *testing.T) *testMocks {
+	ctrl := gomock.NewController(t)
+	teams := mocks.NewMockTeamsService(ctrl)
+	graph := mocks.NewMockGraphQLClient(ctrl)
+	repo := mocks.NewMockRepositoriesService(ctrl)
+	ghClient := &GitHubClient{
+		Teams:        teams,
+		Context:      context.TODO(),
+		Graph:        graph,
+		Repositories: repo,
+	}
+	return &testMocks{
+		ctrl:      ctrl,
+		client:    ghClient,
+		graphMock: graph,
+		repoMock:  repo,
+		teamMock:  teams,
+	}
+}
