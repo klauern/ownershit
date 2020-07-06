c1c244d2317f60b3b2dc66fa2288215b7be730f5
Nick Klauer <nklauer@zendesk.com>
2020-07-05 20:17:22 -0500

    ✅ wildly improve test coverage
    adding mocks and using them all over the place to verify behavior

--- github_v3_test.go
+++ github_v3_test.go
@@ -2,43 +2,15 @@ package ownershit
 
 import (
 	"bytes"
-	"context"
-	"flag"
+	"errors"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-github/v32/github"
 	"github.com/klauern/ownershit/mocks"
-	"github.com/rs/zerolog"
-	"github.com/shurcooL/githubv4"
-	"github.com/stretchr/testify/assert"
 )
-
-func stringPtr(s string) *string {
-	return &s
-}
-
-func boolPtr(b bool) *bool {
-	return &b
-}
-
-func defaultGitHubClient() *GitHubClient {
-	return NewGitHubClient(context.TODO(), "")
-}
-
-var (
-	githubv3 = flag.Bool("githubv3", false, "run GitHub V3 Integration Tests")
-)
-
-func TestMain(m *testing.M) {
-	zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	flag.Parse()
-	result := m.Run()
-	os.Exit(result)
-}
 
 func TestMapPermsWithGitHub(t *testing.T) {
 	if !*githubv3 {
@@ -62,12 +34,6 @@ func TestOmitPermFixes(t *testing.T) {
 	client := defaultGitHubClient()
 	repoSvc := mocks.NewMockRepositoriesService(ctrl)
 	client.Repositories = repoSvc
-
-	defaultGoodResponse := &github.Response{
-		Response: &http.Response{
-			StatusCode: 200,
-		},
-	}
 
 	repoSvc.
 		EXPECT().
@@ -118,15 +84,27 @@ func TestGitHubClient_AddPermissions(t *testing.T) {
 				Body:       ioutil.NopCloser(bytes.NewReader([]byte("something"))),
 			},
 		}, nil)
+	teamMock.
+		EXPECT().
+		AddTeamRepoBySlug(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).
+		Return(&github.Response{
+			Response: &http.Response{
+				StatusCode: 500,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte("error"))),
+			},
+		}, errors.New("dummy error"))
 
 	graphMock := mocks.NewMockGraphQLClient(ctrl)
 
 	type fields struct {
-		Teams   TeamsService
-		Graph   GraphQLClient
-		V3      *github.Client
-		V4      *githubv4.Client
-		Context context.Context
+		Teams TeamsService
+		Graph GraphQLClient
 	}
 	type args struct {
 		repo         string
@@ -142,11 +120,8 @@ func TestGitHubClient_AddPermissions(t *testing.T) {
 		{
 			name: "",
 			fields: fields{
-				Teams:   teamMock,
-				Graph:   graphMock,
-				V3:      nil,
-				V4:      nil,
-				Context: nil,
+				Teams: teamMock,
+				Graph: graphMock,
 			},
 			args: args{
 				repo:         "junk",
@@ -158,59 +133,32 @@ func TestGitHubClient_AddPermissions(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "",
+			fields: fields{
+				Teams: teamMock,
+				Graph: graphMock,
+			},
+			args: args{
+				repo:         "junk",
+				organization: "junk",
+				perm: Permissions{
+					Team:  stringPtr("me"),
+					Level: stringPtr("PUSH"),
+				},
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &GitHubClient{
-				Teams:   tt.fields.Teams,
-				Graph:   tt.fields.Graph,
-				V3:      tt.fields.V3,
-				V4:      tt.fields.V4,
-				Context: tt.fields.Context,
+				Teams: tt.fields.Teams,
+				Graph: tt.fields.Graph,
 			}
 			if err := c.AddPermissions(tt.args.organization, tt.args.repo, &tt.args.perm); (err != nil) != tt.wantErr {
 				t.Errorf("AddPermissions() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
-}
-
-func TestGitHubClient_AddPermissions2(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	teamMock := mocks.NewMockTeamsService(ctrl)
-
-	any := gomock.Any()
-
-	teamMock.
-		EXPECT().
-		AddTeamRepoBySlug(
-			any,
-			any,
-			any,
-			"klauern",
-			"ownershit",
-			&github.TeamAddTeamRepoOptions{Permission: "push"},
-		).
-		Return(&github.Response{
-			Response: &http.Response{
-				StatusCode: 0,
-			},
-		}, nil)
-
-	client := &GitHubClient{
-		Teams:   teamMock,
-		Graph:   nil,
-		V3:      nil,
-		V4:      nil,
-		Context: nil,
-	}
-
-	_, err := client.Teams.AddTeamRepoBySlug(context.Background(), "", "", "klauern", "ownershit", &github.TeamAddTeamRepoOptions{
-		Permission: "push",
-	})
-	if err != nil {
-		t.Error(err)
-	}
-
-	assert.NoError(t, err)
 }
