5139e998c0848064f65af930d303d59cc07ab7c2
Nick Klauer <nklauer@zendesk.com>
2020-10-05 22:47:49 -0500

    ♻️ extract variables out a bit more for archiving cmd

--- cmd/archiving.go
+++ cmd/archiving.go
@@ -17,6 +17,11 @@ var ErrUsernameNotDefined = errors.New("username not defined for query")
 var username string
 
 var client *shit.GitHubClient
+var (
+	forks int
+	stars int
+	days  int
+)
 
 var archiveFlags = []cli.Flag{
 	&cli.StringFlag{
@@ -26,19 +31,22 @@ var archiveFlags = []cli.Flag{
 		Aliases:     []string{"u"},
 	},
 	&cli.IntFlag{
-		Name:  "forks",
-		Usage: "maximum number of forks that a repo can have before it is considered archivalable",
-		Value: 0,
+		Name:        "forks",
+		Usage:       "maximum number of forks that a repo can have before it is considered archivalable",
+		Destination: &forks,
+		Value:       0,
 	},
 	&cli.IntFlag{
-		Name:  "stars",
-		Usage: "maximum number of stars that a repository should have before it is considered archivable",
-		Value: 0,
+		Name:        "stars",
+		Usage:       "maximum number of stars that a repository should have before it is considered archivable",
+		Destination: &stars,
+		Value:       0,
 	},
 	&cli.IntFlag{
-		Name:  "days",
-		Usage: "maximum number of days since last activity before considering for archival",
-		Value: 365,
+		Name:        "days",
+		Usage:       "maximum number of days since last activity before considering for archival",
+		Destination: &days,
+		Value:       365,
 	},
 }
 
@@ -63,8 +71,15 @@ var ArchiveSubcommands = []*cli.Command{
 }
 
 func queryCommand(c *cli.Context) error {
-	log.Info().Msgf("querying with parms")
-	repos, err := client.QueryArchivableIssues(username, c.Int("forks"), c.Int("stars"), c.Int("days"))
+	log.
+		Info().
+		Fields(map[string]interface{}{
+			"forks": forks,
+			"stars": stars,
+			"days":  days,
+		}).
+		Msgf("querying with parms")
+	repos, err := client.QueryArchivableIssues(username, forks, stars, days)
 	if err != nil {
 		return err
 	}
@@ -76,10 +91,10 @@ func queryCommand(c *cli.Context) error {
 
 	repos = shit.SortedRepositoryInfo(repos)
 	for _, repo := range repos {
-		forks := int(repo.ForkCount)
-		stars := int(repo.StargazerCount)
+		repoForks := int(repo.ForkCount)
+		repoStars := int(repo.StargazerCount)
 		lastUpdated := repo.UpdatedAt.Time
-		table.Append([]string{string(repo.Name), strconv.Itoa(forks), strconv.Itoa(stars), lastUpdated.String()})
+		table.Append([]string{string(repo.Name), strconv.Itoa(repoForks), strconv.Itoa(repoStars), lastUpdated.String()})
 	}
 	table.Render()
 	fmt.Println(tableBuf.String())
@@ -88,5 +103,9 @@ func queryCommand(c *cli.Context) error {
 
 func executeCommand(c *cli.Context) error {
 	log.Info().Msgf("executing Archive with parms")
+	_, err := client.QueryArchivableIssues(username, forks, stars, days)
+	if err != nil {
+		return err
+	}
 	return nil
 }
