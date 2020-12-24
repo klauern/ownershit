17788668100a2ca2e08862325876849d581e6113
Nick Klauer <nklauer@zendesk.com>
2020-12-24 16:06:27 -0600

    ⬆️ upgrade deps
    github, survey, oauth2, cli/v2

--- branch.go
+++ branch.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/google/go-github/v32/github"
+	"github.com/google/go-github/v33/github"
 	"github.com/rs/zerolog/log"
 )
 
--- mocking_test.go
+++ mocking_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-github/v32/github"
+	"github.com/google/go-github/v33/github"
 	"github.com/klauern/ownershit/mocks"
 	"github.com/rs/zerolog"
 )
