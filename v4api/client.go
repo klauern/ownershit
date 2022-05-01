afd3a358eecb69e3cbae59a57038158ce47c8757
Nick Klauer <nklauer@zendesk.com>
2022-04-30 19:33:03 -0500

    ✨ use genqlient for label updates

--- v4api/client.go
+++ v4api/client.go
@@ -46,8 +46,12 @@ type Teams []struct {
 }
 
 // Type aliases for genqlient stuff
-type OrganizationTeams []getTeamsOrganizationTeamsTeamConnectionEdgesTeamEdge
-type RateLimit getRateLimitResponse
+type OrganizationTeams []GetTeamsOrganizationTeamsTeamConnectionEdgesTeamEdge
+
+type (
+	RateLimit GetRateLimitResponse
+	Label     GetRepositoryIssueLabelsRepositoryLabelsLabelConnectionEdgesLabelEdgeNodeLabel
+)
 
 func (t *authedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("Authorization", "bearer "+t.key)
@@ -126,8 +130,8 @@ func NewGHv4Client() *GitHubV4Client {
 }
 
 func (c *GitHubV4Client) GetTeams() (OrganizationTeams, error) {
-	orgTeams := []getTeamsOrganizationTeamsTeamConnectionEdgesTeamEdge{}
-	initResp, err := getTeams(c.Context, c.client, TeamOrder{
+	orgTeams := []GetTeamsOrganizationTeamsTeamConnectionEdgesTeamEdge{}
+	initResp, err := GetTeams(c.Context, c.client, TeamOrder{
 		Direction: OrderDirectionDesc,
 		Field:     TeamOrderFieldName,
 	}, 100, "")
@@ -139,7 +143,7 @@ func (c *GitHubV4Client) GetTeams() (OrganizationTeams, error) {
 	cursor := initResp.Organization.Teams.PageInfo.EndCursor
 	for hasNextPage {
 		log.Debug().Str("cursor", cursor).Msg("has next page")
-		resp, err := getTeams(c.Context, c.client, TeamOrder{
+		resp, err := GetTeams(c.Context, c.client, TeamOrder{
 			Direction: OrderDirectionDesc,
 			Field:     TeamOrderFieldName,
 		}, 100, cursor)
@@ -157,7 +161,7 @@ func (c *GitHubV4Client) GetTeams() (OrganizationTeams, error) {
 }
 
 func (c *GitHubV4Client) GetRateLimit() (RateLimit, error) {
-	resp, err := getRateLimit(c.Context, c.client)
+	resp, err := GetRateLimit(c.Context, c.client)
 	if err != nil {
 		return RateLimit{}, fmt.Errorf("can't get ratelimit: %w", err)
 	}
@@ -165,12 +169,55 @@ func (c *GitHubV4Client) GetRateLimit() (RateLimit, error) {
 	return RateLimit(*resp), nil
 }
 
-func (c *GitHubV4Client) UpdateLabels(repo string, labels []Label) error {
-	respositoryIssueLabe
+func (c *GitHubV4Client) SyncLabels(repo string, labels []Label) error {
+	labelsMap := map[string]Label{}
+	labelResp, err := GetRepositoryIssueLabels(c.Context, c.client, repo, "zendesk")
+	repoId := labelResp.Repository.Id
+	if err != nil {
+		return fmt.Errorf("can't get labels: %w", err)
+	}
+	for _, label := range labelResp.Repository.Labels.Edges {
+		labelsMap[label.Node.Name] = Label(label.Node)
+	}
 	for _, label := range labels {
-		_, err := updateLabel(c.Context, c.client, repo, label)
-		if err != nil {
-			return fmt.Errorf("can't update label: %w", err)
+		// create some kind of type to track which are new, missing, or updated
+		l, ok := labelsMap[label.Name]
+		if ok {
+			// Label name exists, so we need to update it
+			log.Debug().Interface("label", label).Msg("label exists")
+			_, err := UpdateLabel(c.Context, c.client, UpdateLabelInput{
+				Name:        l.Name,
+				Color:       l.Color,
+				Description: l.Description,
+				Id:          l.Id,
+			})
+			if err != nil {
+				return fmt.Errorf("can't update label: %w", err)
+			}
+			delete(labelsMap, label.Name)
+		} else {
+			// Label name doesn't exist, so we need to create it
+			log.Debug().Interface("label", label).Msg("label does not exist")
+			_, err := CreateLabel(c.Context, c.client, CreateLabelInput{
+				Name:         l.Name,
+				Color:        l.Color,
+				Description:  l.Description,
+				RepositoryId: repoId,
+			})
+			if err != nil {
+				return fmt.Errorf("can't create label: %w", err)
+			}
+			delete(labelsMap, label.Name)
 		}
 	}
+	// now we have a map of labels that we need to delete
+	for _, label := range labelsMap {
+		_, err := DeleteLabel(c.Context, c.client, DeleteLabelInput{
+			Id: label.Id,
+		})
+		if err != nil {
+			return fmt.Errorf("can't delete label: %w", err)
+		}
+	}
+	return nil
 }
